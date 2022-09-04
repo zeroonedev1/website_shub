dbbf8e588967a03099bc52824b95391139ad7160
Shubham Dholu <shubhamdholu@gmail.com>
2022-09-03 22:21:56 -0400

    Portfolio Component Updates

--- src/components/header/HeaderSoials.jsx
+++ src/components/header/HeaderSoials.jsx
@@ -5,8 +5,8 @@ import {FaGithub} from 'react-icons/fa'
 const HeaderSoials = () => {
   return (
     <div className='header_socials'>
-      <a href="https://linkedin.com" target='_blank'><BsLinkedin/></a>
-      <a href="https://github.com" target='_blank'><FaGithub/></a>
+      <a href="https://www.linkedin.com/in/shubham-dholu-a164348b/" target='_blank' rel="noreferrer"><BsLinkedin/></a>
+      <a href="https://github.com/zeroonedev1" target='_blank' rel="noreferrer"><FaGithub/></a>
       {/* <a href="https://dribble.com" target='_blank'></a> */}
     </div>
   )
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,48 +1,30 @@
 import React from 'react'
 import './projects.css'
-import IMG1 from '../../assets/card.png'
+import IMG1 from '../../assets/p6.png'
+import IMG2 from '../../assets/p1.png'
+import IMG3 from '../../assets/p7.png'
 
 const data = [
   {
     id: 1,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: "https://github.com",
+    image: IMG3,
+    title: 'shadePlace - A Place where people can share underated places to explore in a city.',
+    github: "https://github.com/zeroonedev1/shadePlace",
     demo: 'https://github.com'
   },
   {
     id: 2,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: "https://github.com",
+    image: IMG2,
+    title: 'Stacct - Project Management for C executives suite.',
+    github: "https://github.com/zeroonedev1/PMA_FE",
     demo: 'https://github.com'
-  },
+  }
+  ,
   {
     id: 3,
     image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: "https://github.com",
-    demo: 'https://github.com'
-  },
-  {
-    id: 4,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: "https://github.com",
-    demo: 'https://github.com'
-  },
-  {
-    id: 5,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: "https://github.com",
-    demo: 'https://github.com'
-  },
-  {
-    id: 6,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: "https://github.com",
+    title: 'Employee Speech Support BOT - inprogress',
+    github: "https://github.com/zeroonedev1/voice-recognization-ai",
     demo: 'https://github.com'
   }
 ]
@@ -64,8 +46,8 @@ const Projects = () => {
                 <h3>{title}</h3>
                 <div className="portfolio__item-cta">
                   <a href={github} className = 'btn' target='_blank' rel="noreferrer">Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live
-                    Demo</a>
+                  {/* <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer">Live
+                    Demo</a> */}
                 </div>
               </article>
             )
