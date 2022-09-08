cb83f1f2306339e3c049c0573e4c2216bacb9b5a
Shubham Dholu <shubhamdholu@gmail.com>
2022-09-08 16:17:17 -0400

    footer links fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,14 +5,12 @@ const Footer = () => {
   return (
     <footer>
       <a href="" className='footer__logo'>Shubham Dholu</a>
-
       <ul className='links'>
         <li><a href="#">Home</a></li>
         <li><a href="#abour">About</a></li>
         <li><a href="#xp">Portfolio</a></li>
         <li><a href="#">Contact</a></li>
       </ul>
-
       <div className="footer__note">
       <small>&copy; use everything form here, I really dont mind.</small>
       </div>
