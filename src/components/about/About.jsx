5ab7b90a5ae2e8b298ef1bfbde6333e8021672d8
shubhamdholu <shubhamdholu@gmail.com>
2023-05-21 02:42:39 -0400

    About - removed animation, styling. Updated resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/3_1.png'
-import {TbMilitaryAward} from 'react-icons/tb'
-import {VscOrganization} from 'react-icons/vsc'
-import {RiCodeBoxFill} from 'react-icons/ri'
+import { TbMilitaryAward } from 'react-icons/tb'
+import { VscOrganization } from 'react-icons/vsc'
+import { RiCodeBoxFill } from 'react-icons/ri'
 
 const About = () => {
   return (
@@ -17,39 +17,45 @@ const About = () => {
             <img src={ME} alt="me" />
           </div>
         </div>
-        
+
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
-              <TbMilitaryAward className="about__icon"/>
+              <TbMilitaryAward className="about__icon" />
               <h2>Experience</h2>
-             <p> <small>4 years of Front End Development.</small></p>
-              <p><small>365+ days of Game Development.</small></p>
+              <p><strong>5+</strong><br></br> years of Web/Front End Development.</p>
+              <p><strong>365+</strong><br></br> days of Game Development.</p>
             </article>
 
             <article className='about__card'>
-              <VscOrganization className="about__icon"/>
+              <VscOrganization className="about__icon" />
               <h2>Companies</h2>
-              <p><small><a href="https://zenithsoft.com/">Zenith Software</a></small></p>
-              <p><small><a href="https://eka1.com/">Eka</a></small></p>
-              <p><small><a href="https://moonfroglabs.com/">Moonfrog Labs</a></small></p>
-              <p><small><a href="https://www.slksoftware.com/">SLK Software</a></small></p>
+              <div className='companies_parent'>
+                <p><a href="https://ottpay.com/">OTT Pay</a></p>
+                <p><a href="https://tamwood.com/">Tamwood Careers</a></p>
+                <p><a href="https://zenithsoft.com/">Zenith Software</a></p>
+                <p><a href="https://eka1.com/">Eka Software</a></p>
+                <p><a href="https://moonfroglabs.com/">Moonfrog Labs</a></p>
+                <p><a href="https://www.slksoftware.com/">SLK Software</a></p>
+              </div>
             </article>
 
             <article className='about__card'>
-              <RiCodeBoxFill className="about__icon"/>
+              <RiCodeBoxFill className="about__icon" />
               <h2>Domains</h2>
-              <p><small>BFSI</small></p>
-              <p><small>Gaming</small></p>
-              <p><small>Commodities-Trading</small></p>
-              <p><small>Manufacturing</small></p>
+              <p>BFSI</p>
+              <p>Gaming</p>
+              <p>Commodities/Trading</p>
+              <p>Manufacturing</p>
+              <p>Educational</p>
             </article>
           </div>
 
-          <p>I have reached to a 5th year of my Front-end web development & Game development odyssey, Along the way I have joined forces with 5+ clients to fight their tech-wars & now, I feel I want more!</p>
+          <p className='about_intro'>I have reached to a 5th year of my Web development & Game development odyssey,
+            Along the way I have joined forces with 5+ clients to fight their tech-wars & now, I feel I want more!</p>
           <a href="#contact" className='btn btn-primary'>Lets Talk  </a>
         </div>
-      </div> 
+      </div>
     </section>
   )
 }
